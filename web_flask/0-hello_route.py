62002a024afc412043841ba0e5dbc775374a3667
xeno <xeno@xeno.0.2.15>
2024-01-28 13:53:20 +0100

    Web FLask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"starts a flash web application"
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    "prints a message when / is called"
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    "main function"
     app.run(host='0.0.0.0', port=5000)
