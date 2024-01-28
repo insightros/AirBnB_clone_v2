e70e696cb820af77f3716293ca617a3b34921ec5
xeno <xeno@xeno.0.2.15>
2024-01-28 13:57:50 +0100

    Web FLask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,28 +6,23 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    "prints a message when / is called"
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    "prints a message when /hbnb is called"
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    "prints a message when /c is called"
     return "C " + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is_cool'):
-    "prints a message when /python is called"
     return "Python " + text.replace('_', ' ')
 
 if __name__ == "__main__":
-    "main function"
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,34 +6,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    "prints a message when / is called"
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    "prints a message when /hbnb is called"
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    "prints a message when /c is called"
     return "C " + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is_cool'):
-    "prints a message when /python is called"
     return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_n_number(n):
-    "prints a message when /number is called"
     return "{:d} is a number".format(n)
 
 if __name__ == "__main__":
-    "main function"
     app.run(host='0.0.0.0', port=5000)
