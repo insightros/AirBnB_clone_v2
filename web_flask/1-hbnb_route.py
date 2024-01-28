25567f0e2626a4844fb7e628c1f9236beea60344
xeno <xeno@xeno.0.2.15>
2024-01-28 13:55:26 +0100

    Web FLask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    "prints a message when / is called"
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    "prints a message when /hbnb is called"
     return 'HBNB'
 
 if __name__ == "__main__":
-    "main function"
     app.run(host='0.0.0.0', port=5000)
